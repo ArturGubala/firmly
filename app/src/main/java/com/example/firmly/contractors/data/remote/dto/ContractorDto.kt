00e2173f3f6e251356a57620b2e40b01975b0b64
Artur <gubala-artur@wp.pl>
2024-11-11 20:32:53 +0100

    Add contractor dto and mapper for remote data source.

---
+++ app/src/main/java/com/example/firmly/contractors/data/remote/dto/ContractorDto.kt
@@ -0,0 +1,47 @@
+package com.example.firmly.contractors.data.remote.dto
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
+
+@Serializable
+data class SingleContractorResponse @OptIn(ExperimentalSerializationApi::class) constructor(
+    @JsonNames("firma") val contractor: List<ContractorDto>
+)
+
+@Serializable
+data class MultipleContractorsResponse @OptIn(ExperimentalSerializationApi::class) constructor(
+    @JsonNames("firmy") val contractors: List<ContractorDto>
+)
+
+@Serializable
+data class ContractorDto @OptIn(ExperimentalSerializationApi::class) constructor(
+    val id: String,
+    @JsonNames("nazwa") val name: String,
+    @JsonNames("adresDzialalnosci") val companyAddress: CompanyDetailAddress?,
+    @JsonNames("wlasciciel") val owner: OwnerDetail,
+    @JsonNames("dataRozpoczecia") val startDate: String,
+    @JsonNames("status") val state: String,
+    @JsonNames("telefon") val phone: String?
+)
+
+@Serializable
+data class CompanyDetailAddress @OptIn(ExperimentalSerializationApi::class) constructor(
+    @JsonNames("ulica") val street: String?,
+    @JsonNames("budynek") val building: String?,
+    @JsonNames("lokal") val apartment: String?,
+    @JsonNames("miasto") val city: String?,
+    @JsonNames("wojewodztwo") val province: String?,
+    @JsonNames("powiat") val district: String?,
+    @JsonNames("gmina") val parish: String?,
+    @JsonNames("kraj") val country: String?,
+    @JsonNames("kod") val postalCode: String?
+)
+
+@Serializable
+data class OwnerDetail @OptIn(ExperimentalSerializationApi::class) constructor(
+    @JsonNames("imie") val firstName: String?,
+    @JsonNames("nazwisko") val lastName: String?,
+    @JsonNames("nip") val taxNumber: String,
+    @JsonNames("regon") val buisnessRegistryNumber: String
+)
---
+++ app/src/main/java/com/example/firmly/contractors/data/remote/dto/ContractorMappers.kt
@@ -0,0 +1,49 @@
+package com.example.firmly.contractors.data.remote.dto
+
+import com.example.firmly.core.domain.contractor.ContractorDetail
+import com.example.firmly.core.domain.contractor.ContractorListItem
+
+fun ContractorDto.toContractorListItem(): ContractorListItem {
+    return ContractorListItem(
+        id = id,
+        name = name,
+        street = companyAddress?.street,
+        building = companyAddress?.building,
+        apartment = companyAddress?.apartment,
+        city = companyAddress?.city,
+        postalCode = companyAddress?.postalCode,
+        taxNumber = owner.taxNumber,
+        buisnessRegistryNumber = owner.buisnessRegistryNumber
+    )
+}
+
+fun ContractorDto.toContractorDetail(): ContractorDetail {
+    return ContractorDetail(
+        id = id,
+        name = name,
+        street = companyAddress?.street,
+        building = companyAddress?.building,
+        apartment = companyAddress?.apartment,
+        city = companyAddress?.city,
+        district = companyAddress?.district,
+        parish = companyAddress?.parish,
+        country = companyAddress?.country,
+        postalCode = companyAddress?.postalCode,
+        firstName = owner.firstName,
+        lastName = owner.lastName,
+        taxNumber = owner.taxNumber,
+        buisnessRegistryNumber = owner.buisnessRegistryNumber,
+        startDate = startDate,
+        state = state,
+        phone = phone,
+        temporary = 0
+    )
+}
+
+fun SingleContractorResponse.toContractorDetail(): List<ContractorDetail> {
+    return contractor.map { it.toContractorDetail() }
+}
+
+fun MultipleContractorsResponse.toContractorListItems(): List<ContractorListItem> {
+    return contractors.map { it.toContractorListItem() }
+}
