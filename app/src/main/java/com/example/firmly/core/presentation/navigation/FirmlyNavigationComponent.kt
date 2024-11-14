d246bc64daedac350df8f615aa73f74c1c26e165
Artur <gubala-artur@wp.pl>
2024-11-14 23:52:05 +0100

    First implementation of top app bar.

--- app/src/main/java/com/example/firmly/core/presentation/navigation/FirmlyNavigationComponent.kt
+++ app/src/main/java/com/example/firmly/core/presentation/navigation/FirmlyNavigationComponent.kt
@@ -1,9 +1,20 @@
 package com.example.firmly.core.presentation.navigation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldLayout
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
 import androidx.compose.runtime.Composable
@@ -11,8 +22,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.testTag
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FirmlyNavigationWrapper(
     currentDestination: NavDestination?,
@@ -29,7 +43,41 @@ fun FirmlyNavigationWrapper(
             )
         }
     ) {
-        content()
+        Scaffold(
+            modifier = Modifier,
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = { Text(text = currentDestination?.route?.split(".")?.last() ?: "Top App Bar") },
+                    navigationIcon = {
+                        IconButton(onClick = { }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = "",
+                                tint = MaterialTheme.colorScheme.onSurface,
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = {  }) {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = "",
+                                tint = MaterialTheme.colorScheme.onSurface,
+                            )
+                        }
+                    },
+                    modifier = Modifier.testTag("firmlyTopAppBar"),
+                )
+            }
+        ) { values ->
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(values)
+            ) {
+                content()
+            }
+        }
     }
 }
 
