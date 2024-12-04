35659022ec3449a2960fd0aa6d4fe359c848303d
arturgie <gubala-artur@wp.pl>
2024-12-04 19:23:29 +0100

    Add event observer and loading view for ContractorListScreen.

--- app/src/main/java/com/example/firmly/contractors/presentation/contractor_list/ContractorListScreen.kt
+++ app/src/main/java/com/example/firmly/contractors/presentation/contractor_list/ContractorListScreen.kt
@@ -1,6 +1,10 @@
 package com.example.firmly.contractors.presentation.contractor_list
 
 import android.annotation.SuppressLint
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -8,9 +12,11 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,10 +24,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.example.firmly.core.presentation.components.FirmlyTopAppBar
 import com.example.firmly.core.presentation.navigation.TopLevelDestination
+import com.example.firmly.core.presentation.util.ObserveAsEvents
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,6 +40,22 @@ internal fun ContractorsListRoute(
     viewModel: ContractorListViewModel = koinViewModel()
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
+
+    val context = LocalContext.current
+    ObserveAsEvents(viewModel.events) { event ->
+        when (event) {
+            is ContractorListEvent.Error -> {
+                Toast.makeText(
+                    context,
+                    event.error,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            is ContractorListEvent.NavigateToDetail -> {
+//                navController.navigateToSearchDetail(event.contractorId)
+            }
+        }
+    }
 
     ContractorListScreen(
         onBackClick = onBackClick,
@@ -68,14 +92,38 @@ private fun ContractorListScreen(
         },
         content = { padding ->
 
-            LazyColumn(
-                modifier = Modifier
-                    .padding(padding)
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                items(state.contractors) { contractor ->
-                    Text(text = contractor.name)
+            if (state.isLoading) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            } else {
+                LazyColumn(
+                    modifier = Modifier
+                        .padding(padding)
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    items(state.contractors) { contractor ->
+                        ListItem(
+                            modifier = Modifier.clickable {
+                                onAction(ContractorListAction.OnContractorCardClick(contractor.id))
+                            },
+                            headlineContent = {
+                                Text(text = contractor.name)
+                            },
+                            supportingContent = {
+                                Row {
+                                    Text(text = "NIP: ${contractor.taxNumber}")
+                                    Text(text = " | ")
+                                    Text(text = "REGON: ${contractor.buisnessRegistryNumber}")
+                                }
+                            }
+                        )
+                    }
                 }
             }
         }
