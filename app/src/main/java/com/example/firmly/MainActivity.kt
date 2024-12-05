55995f5b827cabb49f22be244abb713ba0d8dce4
arturgie <gubala-artur@wp.pl>
2024-12-05 16:47:50 +0100

    Fix for different navigation system bar. Don't know if it is correct way of doing that. For now it works.

--- app/src/main/java/com/example/firmly/MainActivity.kt
+++ app/src/main/java/com/example/firmly/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.firmly
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.firmly.ui.theme.FirmlyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+//            statusBarStyle = SystemBarStyle.dark(0x00000000),
+            navigationBarStyle = SystemBarStyle.dark(0x00000000)
+        )
         super.onCreate(savedInstanceState)
 
         setContent {
